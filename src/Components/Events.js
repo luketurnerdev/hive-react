5ff291fb7f5041656cccbc578aad22a697e15bc7
Dodam <lapeconera@gmail.com>
2019-07-12 11:34:43 +1000

    installed fakejason and logo for homepage

---
+++ src/Components/Events.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+// import axios for sending requests to API
+import axios from 'axios';
+
+// Events Component
+class Events extends Component {
+  state={
+    events:[]
+  }
+
+  // just after rendering the Events, call to the API
+  componentDidMount(){
+  axios
+      .get('http://localhost:3000/events')
+      .then(resp => {
+          console.log(resp.data)
+          const {data} = resp;
+          // modify the state according to the data in the API's response
+         this.setState({events:data})
+        
+      })
+      .catch(error => {
+          console.log(error);
+      });  
+    
+  }
+
+render(){
+  console.log(this.state.events)
+  const {events} = this.state
+  return( 
+    <div><h1>{events.map((event)=>(<div key={event.id} >{event.name}</div>))}</h1></div>
+  )
+
+}
+
+
+};
+
+export default Events;
---
+++ src/Components/Test.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export class Test extends Component {
+    state = {
+        users:[]
+    }
+    componentDidMount(){
+        axios.get('http://localhost:3000/users')
+        .then(resp=>{
+            const {data} = resp;
+            this.setState({users:data});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        
+    const {users} = this.state
+    
+        return (
+        <ul>{users.map((user)=> ( <li key={user.id}>{user.name}</li>))}</ul>
+        )
+    
+                
+            }
+}
+
+export default Test;
+
+
+
+
+
