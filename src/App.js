956ca5bf29a7435339dac0ef91a549c7ecfab4cb
Joshua-Tu <tjh8523@gmail.com>
2019-07-15 11:04:58 +1000

    components only missing statistic and calendar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import { BrowserRouter, Route } from "react-router-dom";
 // import HomePage from './Components/pages/HomePage'
 import DbEvents from './Components/events/DbEvents'
 import './App.css';
+import Profile from './Components/profiles/Profile'
 
 class App extends Component {
   render() {
       return (
-        <div>
-              <BrowserRouter>
-                  <div>
-                      {/* <Route exact path="/" component={HomePage} />
-                      <Route exact path="/login" component={Login} />
-                      <Route exact path="/requestaccess" component={RequestAccess} />
-                      <Route exact path="/dashboard" component={Dashboard} /> */}
-                      <Route exact path="/events" component={DbEvents} />
+        // <div>
+        //       <BrowserRouter>
+        //           <div>
+        //               {/* <Route exact path="/" component={HomePage} />
+        //               <Route exact path="/login" component={Login} />
+        //               <Route exact path="/requestaccess" component={RequestAccess} />
+        //               <Route exact path="/dashboard" component={Dashboard} /> */}
+        //               <Route exact path="/events" component={DbEvents} />
 
 
 
                   
                   
-                  </div>
-              </BrowserRouter>
-          </div>
+        //           </div>
+        //       </BrowserRouter>
+        //   </div>
+        <Profile />
       );
   }
 }
