ee185d2913323c34e944c7e88d39f697c86bbbe9
Liah K <lapeconera@gmail.com>
2019-07-24 08:49:48 +1000

    fixed login and homepage ( added login button)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,44 @@
 // Output: FAQ and the basic instruction of the platfomr
 // not Signed in.
-import React from 'react';
+import React, { Component } from "react";
 import logo from './honeycomb-logo.svg';
+import {Col,Container,Button}  from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
 
-function HomePage() {
-  return (
+const WrapperLogin = styled.section`
+  padding: 2em;
+  margin:6em;
+  display:flex;
+  align-items:center
+`;
+
+class HomePage extends Component {
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+     
+        <Container>             
+        <WrapperLogin>
+        <Col>
+            <div>
+                <a href={`${process.env.REACT_APP_BACKEND_API}/auth/meetup`}>
+                <Button size="sm" variant="primary">Login</Button>
+                </a>
+                <Link to="/users/request">
+                <Button size="sm" variant="link">RequestAccess</Button>
+                </Link>
+            </div>
+        </Col>
+        </WrapperLogin>
+        </Container>
+        </header>
+  </div>
+  )
+  };
 }
 
 export default HomePage;
