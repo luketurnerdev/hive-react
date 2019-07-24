8e624ebc8a51d8b61b9ec388ab215cecfca8c19a
Luke Turner <luke@lukedturner.com>
2019-07-24 17:03:28 +1000

    juan fixing error

--- src/Components/CarouselBox/CAEventsBox.js
+++ src/Components/CarouselBox/CAEventsBox.js
@@ -126,17 +126,20 @@ class CAEventsBox extends Component {
                             <Col>
                               <Card.Text className="mb-2 text-muted"><small>{item.local_date}</small></Card.Text>
                             </Col>                          
-                            <Col>
+                            <Col>                            
                             <Button size="sm" variant="primary" onClick={()=>this.handleAttend(item._id)}>
                               {!(item.hive_attendees.includes(user._id))?
                               <>Attend</>:
                               <>Unattend</>}
                             </Button>
+                            </Col>
                           {/* START show DELETE button if is admin . otherwise show SUGGEST button */}
                             {/* {user.admin === true?                                                 
                               <Button size="sm" variant="info" value={item._id} onClick={() => this.handleChange(item._id)}>Delete</Button>
                               :null
-                             }                              */}
+                             }   
+                                                        */}
+                            <Col>
                              {!item.suggested.is_suggested?
                              <Button size="sm" variant="info" value={item._id} onClick={() => this.handleChange(item._id)}>Delete</Button>:
                               null
