b3c356244f590012b9897fe3ecbc35832570d5b7
Luke Turner <luke@lukedturner.com>
2019-07-24 12:35:56 +1000

    fixing notdbevents

--- src/Components/events/NotDbEvents.js
+++ src/Components/events/NotDbEvents.js
@@ -74,7 +74,7 @@ export class NotDbEvents extends Component {
                         
                         
                         <>
-                        {console.log(event)}
+                        {console.log(event.id)}
                         <h5 key={event.id}>{event.name} </h5>
                         <li 
                         key={event.id}>{event.local_date} at {event.local_time}
